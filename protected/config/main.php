5266e9308b13a261dc98020469ba24343b7e5aaf
jonathan <jonathancastro2018@yahoo.com>
2019-10-19 16:31:02 +0200

    creando un controlador

--- protected/config/main.php
+++ protected/config/main.php
@@ -8,6 +8,7 @@
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'Jdc Blog',
+	'defaultController'=>'blog',//cambio el controlador por defecto
 
 	// preloading 'log' component
 	'preload'=>array('log'),
